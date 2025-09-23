8113880dab82aa6aea0423b7bc0e99b26ffaa355
Lucien Murhula Kalala <murhulakalalalucien14@gmail.com>
2025-09-23 23:11:20 +0200

    new change for test

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 
 const fs = require('fs');
 const https = require('https');
-const http = require('http');
 const express = require('express');
 const app = express();
 const socketio = require('socket.io');
@@ -21,7 +20,7 @@ app.use(express.static(__dirname))
 //we changed our express setup so we can use https
 //pass the key and cert to createServer on https
 // const expressServer = https.createServer({key, cert}, app);
-const expressServer = http.createServer(app);
+const expressServer = https.createServer(app);
 //create our socket.io server... it will listen to our express port
 const io = socketio(expressServer,{
     cors: {
