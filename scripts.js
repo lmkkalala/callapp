97d34462428a1ddb04b48f2ca7be6e58be357739
Lucien Murhula Kalala <murhulakalalalucien14@gmail.com>
2025-09-30 21:35:02 +0200

    check again

--- scripts.js
+++ scripts.js
@@ -52,8 +52,7 @@ let peerConfiguration = {
         {
             urls:[
               'stun:stun.l.google.com:19302',
-              'stun:stun1.l.google.com:19302',
-              'stun:stun2.l.google.com:19302'
+              'stun:stun1.l.google.com:19302'
             ]
         }
         // ,
